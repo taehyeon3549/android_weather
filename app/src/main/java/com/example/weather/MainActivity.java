0b6b02c73acaa8e896f85e6ad5c508d026eacf92
win_faw <xogusrla09@gmail.com>
2019-12-20 16:32:01 +0900

    현재 위치 위도 경도 x y 값으로 변경 성공

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -11,11 +19,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.weather.WeatherAPI.LocationCodeFetcher;
 import com.example.weather.WeatherAPI.Pin;
 import com.example.weather.WeatherAPI.WeatherFetcher;
 import com.example.weather.WeatherAPI.WeatherSet;
+import com.example.weather.cLocation.ConvertLatLon;
 
 import org.apache.log4j.chainsaw.Main;
 
@@ -25,14 +35,16 @@ public class MainActivity extends AppCompatActivity {
     Pin pin = null;
     Button bt_setAlarm, bt_search;
     ImageView iv_weather;
+    TextView tv_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bt_setAlarm = (Button)findViewById(R.id.bt_setAlarm);
-        bt_search = (Button)findViewById(R.id.bt_search);
+        bt_setAlarm = (Button) findViewById(R.id.bt_setAlarm);
+        bt_search = (Button) findViewById(R.id.bt_search);
+        tv_location = (TextView)findViewById(R.id.tv_location);
 
         bt_setAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /***  지역 위치 설정 ***/
         String[] location = {"대구광역시", "동구", "안심1동"};
         WeatherSet weather = null;
         LocationCodeFetcher lcf = new LocationCodeFetcher();
@@ -59,35 +72,32 @@ public class MainActivity extends AppCompatActivity {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 정각");
         pin = lcf.fetchLocationCode(location);
 
-        //날씨 정보 생성
+        /***  날씨 정보 생성 ***/
         try {
             weather = wf.fetchWeather(pin.getSx(), pin.getSy());
-        }catch (Exception E){
-            Log.i("TEST", E.toString());
+        } catch (Exception E) {
+            Log.i("TEST", "날씨 정보 파싱 에러 : " + E.toString());
         }
-        // 아이콘 설정
+
+        /***  날씨 아이콘 설정 ***/
         weather.weatherIcon(this);
         // TV 설정
-        TextView tw_weather = (TextView)findViewById(R.id.tw_weather);
+        TextView tw_weather = (TextView) findViewById(R.id.tw_weather);
         tw_weather.setText(sdf.format(weather.getBaseDate()) + "의 비/눈 상황은 " + weather.getPty() + ", 하늘은 " + weather.getSky() + "입니다");
 
-        SharedPreferences alarmPreferences =  PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences alarmPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = alarmPreferences.edit();
         editor.clear();
         editor.commit();
 
-        Log.i("TEST", "발표시각 : " + sdf.format(weather.getBaseDate()));
+        /***  위치값 가져오기 ***/
+        GetLocation mylocation = new GetLocation();
+        mylocation.StartGetLocation(this);
+
+        //Log.i("TEST", "발표시각 : " + sdf.format(weather.getBaseDate()));
 //        Log.i("TEST", sdf.format(weather.getFcstDate()) + "의 비/눈 상황은 " + weather.getPty() + ", 하늘은 " + weather.getSky() + "입니다");
 //
-//        //주소 변경 부분
-//        Intent intent = getIntent(); /*데이터 수신*/
-//        try{
-//            Log.d("test",intent.getExtras().getString("address"));
-//            Log.d("test",intent.getExtras().getString("x"));
-//            Log.d("test",intent.getExtras().getString("y"));
-//        }catch (Exception E){
-//            Log.i("test", E.toString());
-//        }
+
 
     }
 
@@ -95,32 +105,116 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        // sharedPreferens 확인
-        SharedPreferences alarmPreferences =  PreferenceManager.getDefaultSharedPreferences(this);
-        // 알람 있는지 유무 체크
-        if(alarmPreferences.getAll().size() == 0) {
+
+        /***  sharedPreferens 확인 ***/
+        SharedPreferences alarmPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        /***  알람 있는지 유무 체크 ***/
+        if (alarmPreferences.getAll().size() == 0) {
             // 저장된 알람이 없음
             Log.i("TEST", "저장된 알람 없음");
-        }else{
+        } else {
             Log.i("TEST", "저장된 알람 있음" + alarmPreferences.getAll().size() + "  시간" + alarmPreferences.getAll());
         }
 
-        //주소 변경 부분
-        Intent intent = getIntent(); /*데이터 수신*/
-        try{
-            Log.d("test",intent.getExtras().getString("address"));
-            Log.d("test",intent.getExtras().getString("x"));
-            Log.d("test",intent.getExtras().getString("y"));
-        }catch (Exception E){
-            Log.i("test", E.toString());
+        /***  주소 변경 부분 ***/
+        Intent intent = getIntent();
+        if(intent.hasExtra("address")){
+            Log.d("test", intent.getExtras().getString("address"));
+            Log.d("test", intent.getExtras().getString("x"));
+            Log.d("test", intent.getExtras().getString("y"));
+        }else{
+            Log.i("test", "x y 변경값 없음");
         }
-//
-//        if(intent.hasExtra("address")){
-//
-//        }else{
-//            Log.i("test", "x y 변경값 없음");
-//        }
-
 
     }
+
+    /***************************************
+    *  메인 엑티비티 위치 값 찍음
+    * **************************************/
+    public void displayLocation(String val){
+        tv_location.setText(val);
+    }
+
+    /***************************************
+     *  현재 위치 값 가져옴
+     * **************************************/
+    public class GetLocation {
+        LocationManager locationManager;
+        private int xPin, yPin;
+        private double longitude, latitude;
+        String provider;
+
+        public void StartGetLocation(Context context) {
+            locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+            // 앱 퍼미션 확인
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    Activity#requestPermissions
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,1, mLocationListener);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000,1, mLocationListener);
+            }
+        }
+
+        public int getxPin() {
+            return xPin;
+        }
+
+        public int getyPin() {
+            return yPin;
+        }
+
+        public double getLat() {
+            return latitude;
+        }
+
+        public double getLon() {
+            return longitude;
+        }
+
+        public String getProvider() {
+            return provider;
+        }
+
+        private final LocationListener mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                /***  위치 값이 갱신 되면 이벤트 발생 ***/
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+                provider = location.getProvider();
+
+                /***  위도 경도를 x,y로 변환 ***/
+                ConvertLatLon convertLatLon = new ConvertLatLon((float)longitude, (float)latitude);
+
+                xPin = convertLatLon.getX();
+                yPin = convertLatLon.getY();
+
+                /***  메인 액티비티의 Textview의 값을 변경 ***/
+                displayLocation("위도 : " + longitude + " 경도 : " + latitude + "X 값 : " + convertLatLon.getX() + " Y 값 :" + convertLatLon.getY() + " \n서비스 제공자" + provider);
+
+                Toast.makeText(MainActivity.this, "위도 : " + longitude + " 경도 : " + latitude + " \n서비스 제공자" + provider, Toast.LENGTH_LONG).show();
+                Log.i("TEST", "위도 : " + longitude + " 경도 : " + latitude + "X 값 : " + convertLatLon.getX() + " Y 값 :" + convertLatLon.getY() + " \n서비스 제공자" + provider);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+    }
+
+
 }
---
+++ app/src/main/java/com/example/weather/cLocation/ConvertLatLon.java
@@ -0,0 +1,147 @@
+package com.example.weather.cLocation;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import static java.lang.Math.asin;
+import static java.lang.Math.cos;
+import static java.lang.Math.log;
+import static java.lang.Math.sin;
+import static java.lang.Math.tan;
+import static java.lang.StrictMath.pow;
+
+public class ConvertLatLon {
+    public final static int NX = 149;       // X 축 격자점 수
+    public final static int NY = 253;       // Y 축 격자점 수
+
+    private ArrayList<Float> xyPin;
+
+    // 파라미터 클래스
+    class LamcParameter{
+        float  Re;          /* 사용할 지구반경 [ km ]      */
+        float  grid;        /* 격자간격        [ km ]      */
+        float  slat1;       /* 표준위도        [degree]    */
+        float  slat2;       /* 표준위도        [degree]    */
+        float  olon;        /* 기준점의 경도   [degree]    */
+        float  olat;        /* 기준점의 위도   [degree]    */
+        float  xo;          /* 기준점의 X좌표  [격자거리]  */
+        float  yo;          /* 기준점의 Y좌표  [격자거리]  */
+        int    first;       /* 시작여부 (0 = 시작)         */
+
+        LamcParameter(float  Re, float  grid, float  slat1, float  slat2, float  olon, float  olat, float  xo, float  yo, int first){
+            this.Re = Re;
+            this.grid = grid;
+            this.slat1 = slat1;
+            this.slat2 = slat2;
+            this.olon = olon;
+            this.olat = olat;
+            this.xo = xo;
+            this.yo = yo;
+            this.first = first;
+        }
+
+        public float getGrid(){
+            return this.grid;
+        }
+    }
+
+    // 생성자
+    public ConvertLatLon(float longitude, float latitude){
+        float lon = longitude;
+        float lat = latitude;
+
+        //
+        //  동네예보 지도 정보
+        //
+        float Re = (float) 6371.00877;     // 지도반경
+        float grid  = (float) 5.0;            // 격자간격 (km)
+        float slat1 = (float) 30.0;           // 표준위도 1
+        float slat2 = (float) 60.0;           // 표준위도 2
+        float olon  = (float) 126.0;          // 기준점 경도
+        float olat  = (float) 38.0;           // 기준점 위도
+        float xo    = 210/grid;   // 기준점 X좌표
+        float yo    = 675/grid;   // 기준점 Y좌표
+        int first = 0;
+
+        // 기본 파라미터 생성
+        LamcParameter lamcParameter = new LamcParameter(Re, grid, slat1, slat2, olon, olat, xo, yo, first);
+
+        // 변환 시작
+        xyPin = map_conv(lon, lat, lamcParameter);
+    }
+
+    public int getX(){
+        int X = Math.round(xyPin.get(0));
+        return X;
+    }
+
+    public int getY(){
+        int Y = Math.round(xyPin.get(1));
+        return Y;
+    }
+
+
+     //좌표변환
+    ArrayList<Float> map_conv
+    (
+            float  lon,                    // 경도(degree)
+            float  lat,                    // 위도(degree)
+            LamcParameter map       // 지도정보
+    ) {
+        float  lon1, lat1;
+
+        lon1 = lon;
+        lat1 = lat;
+
+        ArrayList<Float> result = lamcproj(lon1, lat1, map);
+
+        result.set(0, (float)(result.get(0) + 1.5));
+        result.set(0, (float)(result.get(1) + 1.5));
+
+        return result;
+    }
+
+     //좌표변환 공식
+    ArrayList<Float> lamcproj(float lon, float lat, LamcParameter map){
+        ArrayList<Float> convert = new ArrayList<>();
+
+        double  PI, DEGRAD;
+        double  re = 0, olon, olat, sn, sf, ro = 0;
+        double  slat1, slat2, alon, alat, xn, yn, ra, theta;
+
+        if(map.first == 0) {
+            PI = asin(1.0)*2.0;
+            DEGRAD = PI/180.0;
+
+            re = map.Re/map.grid;
+            slat1 = map.slat1 * DEGRAD;
+            slat2 = map.slat2 * DEGRAD;
+            olon = map.olon * DEGRAD;
+            olat = map.olat * DEGRAD;
+
+            sn = tan(PI*0.25 + slat2*0.5)/tan(PI*0.25 + slat1*0.5);
+            sn = log(cos(slat1)/cos(slat2))/log(sn);
+            sf = tan(PI*0.25 + slat1*0.5);
+            sf = pow(sf,sn)*cos(slat1)/sn;
+            ro = tan(PI*0.25 + olat*0.5);
+            ro = re*sf/pow(ro,sn);
+
+            map.first = 1;
+
+            ra = tan(PI*0.25+(lat)*DEGRAD*0.5);
+            ra = re*sf/pow(ra,sn);
+            theta = (lon)*DEGRAD - olon;
+            if (theta >  PI) theta -= 2.0*PI;
+            if (theta < -PI) theta += 2.0*PI;
+            theta *= sn;
+
+            convert.add(0, (float) (ra*sin(theta)) + map.xo);
+            convert.add(1,(float)(ro - ra*cos(theta)) + map.yo);
+        }
+
+        return convert;
+    }
+
+
+}
